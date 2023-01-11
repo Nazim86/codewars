7df61007e88ecea140ecffa216aa1d3113c5e226
Nazim86 <43852326+Nazim86@users.noreply.github.com>
2023-01-11 23:24:34 +0400

    Create Find the next perfect square.js

---
+++ Find the next perfect square.js
@@ -0,0 +1,11 @@
+function findNextSquare(sq) {
+  // Return the next square if sq is a perfect square, -1 otherwise
+  let sqrt = Math.sqrt(sq);
+  
+  if (Number.isInteger(sqrt)){
+    
+    return (sqrt+1)**2;
+  }
+  
+  return -1;
+}
