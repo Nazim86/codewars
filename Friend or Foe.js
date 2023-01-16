704f2f9f041bb3a8596155e1efd9e590de84a2c6
Nazim86 <43852326+Nazim86@users.noreply.github.com>
2023-01-16 20:42:48 +0400

    Update Friend or Foe.js

--- Friend or Foe.js
+++ Friend or Foe.js
@@ -1,16 +1,15 @@
-unction friend(friends){
+function friend(friends){
   //your code here
   
+  let friend_list = [];
+  
+  
 for (var i=0; i<friends.length; i++){
-  if(parseInt(friends[i])){
-    friends.splice(i,i);
-    i--;
+  
+    if (friends[i].length===4){
+    friend_list.push(friends[i]);
+  
+  }  
   }
-  else if (friends[i].length>4){
-    friends.splice(i,i);
-    i--;
-  }
-  }
-  return friends;
+  return friend_list;
 }
-
